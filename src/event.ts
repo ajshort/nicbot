818be785f863d0654640fd8229bb0b00e5f05c32
Andrew Short <andrewjshort@gmail.com>
2019-03-07 16:18:09 +1100

    Respond to messages

--- src/event.ts
+++ src/event.ts
@@ -1,8 +1,31 @@
+import { BOT_ID, BOT_USER_ID } from "./config";
+
+import { WebClient } from "@slack/client";
 import { createEventAdapter } from "@slack/events-api";
 
 const events = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
-events.on("message", console.log);
+events.on("message", async (event) => {
+  // Ignore messages from ourselves.
+  if (event.bot_id === BOT_ID) {
+    return;
+  }
+
+  const direct = event.channel_type === "im";
+  const mentioned = event.text.includes(`<@${BOT_USER_ID}>`);
+
+  if (!direct && !mentioned) {
+    return;
+  }
+
+  const client = new WebClient(process.env.SLACK_BOT_ACCESS_TOKEN);
+
+  await client.chat.postMessage({
+    channel: event.channel,
+    text: JSON.stringify(event),
+  });
+});
+
 events.on("error", console.error);
 
 export const handler = events.expressMiddleware();
