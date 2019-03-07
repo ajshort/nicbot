b84b142e44a7ea7f5b738af8ab916785745cd247
Andrew Short <andrewjshort@gmail.com>
2019-03-07 16:14:11 +1100

    Constants

---
+++ src/config.ts
@@ -0,0 +1,31 @@
+export const BOT_ID = "BGM9LDYLF";
+export const BOT_USER_ID = "UGMLA582J";
+
+export const CHANNELS = {
+  "bot-testing": "GGMB2GX18",
+  "sms-gateway": "CA6D8RBA5",
+};
+
+export const VEHICLES = [
+  "Ark Angel",
+  "Cage Trailer",
+  "DPT31",
+  "DPT56",
+  "G4",
+  "K4",
+  "Storm Trailer",
+  "WGC19",
+  "WOL32",
+  "WOL33",
+  "WOL39",
+  "WOL43",
+  "WOL55",
+  "WOL56",
+  "WOL57",
+];
+
+export const DUTY_OFFICERS = [
+  "Day DO",
+  "DOO",
+  "Night DO",
+];
