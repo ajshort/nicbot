989d993e4c16d23e730b8dc5f0335a75befc86f1
Andrew Short <andrewjshort@gmail.com>
2019-04-05 16:18:12 +1100

    Use delayed response

--- src/slash-command.ts
+++ src/slash-command.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { RequestHandler } from "express";
 import { getPublicGif, RADARS } from "./bom";
 
@@ -5,14 +6,15 @@ export const handler: RequestHandler = async (req, res) => {
   const data = req.body;
 
   if (data.command === "/radar") {
+    res.status(200).send("Getting your radar image (beep beep beep) :satellite_antenna:");
+
     const input = parseInt(data.text, 10);
     const range = (input in RADARS) ? input : 128;
 
-    // Create the gif - this can take a while.
     const id = RADARS[range];
     const gif = await getPublicGif(id);
 
-    res.json({
+    await axios.post(data.response_url, {
       attachments: [{
         fallback: `http://www.bom.gov.au/products/${id}.loop.shtml`,
         image_url: gif,
@@ -22,10 +24,11 @@ export const handler: RequestHandler = async (req, res) => {
       response_type: "in_channel",
     });
   } else if (data.command === "/wind") {
-    // Create the gif - this can take a while.
+    res.status(200).send("Getting your radar image (beep beep beep) :satellite_antenna:");
+
     const gif = await getPublicGif(RADARS.wind);
 
-    res.json({
+    await axios.post(data.response_url, {
       attachments: [{
         fallback: `http://www.bom.gov.au/products/${RADARS.wind}.loop.shtml`,
         image_url: gif,
