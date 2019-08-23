c3a03303913e579edea7f0e3051453a8aca443ea
Andrew Short <andrewjshort@gmail.com>
2019-08-23 10:13:33 +1000

    Remove vehicle tracking

--- src/config.js
+++
@@ -1,8 +0,0 @@
-exports.BOT_ID = "BGM9LDYLF";
-exports.BOT_USER_ID = "UGMLA582J";
-
-exports.CHANNELS = {
-  "bot-testing": "GGMB2GX18",
-  "dutyofficer-support": "CDFDMRHB8",
-  "sms-gateway": "CA6D8RBA5",
-};
